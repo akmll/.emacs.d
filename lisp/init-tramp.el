69a8cf8ee16a52599a6f81710ff68203765bfeb0
Ma Liangliang <akml@163.com>
2023-06-21 09:05:39 +0800

    Enable lsp-mode with clangd in tramp mode

--- lisp/init-tramp.el
+++ lisp/init-tramp.el
@@ -9,4 +9,11 @@
 
 (provide 'init-tramp)
 
+(require 'lsp-mode)
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
+                  :major-modes '(c-mode c++-mode)
+                  :remote? t
+                  :server-id 'clangd-remote))
+
 ;;; init-tramp.el ends here
