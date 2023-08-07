ff4e739a37e19ec95b3d643b087bed64934203f1
Ma Liangliang <akml@163.com>
2023-08-07 09:02:27 +0800

    Swith color theme to `spacemacs-dark`

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -5,8 +5,9 @@
 (require-package 'zenburn-theme)
 (require-package 'solarized-theme)
 (require-package 'afternoon-theme)
+(require-package 'spacemacs-theme)
 
-(load-theme 'solarized-zenburn t)
+(load-theme 'spacemacs-dark)
 
 (provide 'init-theme)
 ;; init-theme.el ends here
